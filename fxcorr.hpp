a57687650bd7e6824ed0d6f8ef9fe9471874e59f
Timofey <timofey@umnik-22.04>
2023-04-26 13:46:55 +0300

    Added xcorr opeator()

--- fxcorr.hpp
+++ fxcorr.hpp
@@ -1,11 +1,13 @@
 #include "fft.hpp"
 #include "vector_util.hpp"
 
+#include <cstddef>
+
 namespace pcx {
 
 template<typename T,
          std::size_t PackSize = pcx::default_pack_size<T>,
-         typename Allocator   = pcx::aligned_allocator<T, std::align_val_t{64}>>
+         typename Allocator   = pcx::aligned_allocator<T>>
 class fxcorr_unit {
     using real_type      = T;
     using allocator_type = Allocator;
@@ -16,12 +18,10 @@ public:
     fxcorr_unit(pcx::vector<T> g, std::size_t fft_size, allocator_type allocator = allocator_type{})
     : m_fft(fft_size, 2048, allocator)
     , m_kernel(fft_size, allocator)
-    , m_tmp(fft_size, allocator) {
+    , m_tmp(fft_size, allocator)
+    , m_overlap(g.size() - 1) {
         m_fft.unsorted(m_kernel, g);
-        m_kernel    = conj(m_kernel / static_cast<T>(fft_size));
-        auto l_size = (g.size() + PackSize - 1) / PackSize * PackSize + 1;
-        m_overlap   = l_size - 1;
-        auto m_save = fft_size - m_overlap;
+        m_kernel = m_kernel / static_cast<T>(fft_size);
     };
 
     fxcorr_unit(const fxcorr_unit& other)     = default;
@@ -32,9 +32,24 @@ public:
     fxcorr_unit& operator=(const fxcorr_unit& other)     = delete;
     fxcorr_unit& operator=(fxcorr_unit&& other) noexcept = delete;
 
-    void operator()(pcx::vector<T> f) {
-        m_fft.unsorted(m_tmp, f);
-        m_tmp = m_tmp * m_kernel;
+    template<typename VAllocator, std::size_t VPackSize>
+    void operator()(pcx::vector<T, VAllocator, VPackSize>& vec) {
+        uint offset = 0;
+        auto step   = (m_fft.size() - m_overlap) / VPackSize * VPackSize;
+        for (; offset + m_fft.size() < vec.size(); offset += step) {
+            m_fft(m_tmp, subrange(vec.begin() + offset, m_fft.size()));
+            m_tmp = m_tmp * conj(m_kernel);
+            m_fft.ifft(m_tmp);
+            subrange(vec.begin() + offset, step).assign(m_tmp);
+        }
+        while (offset < vec.size()) {
+            m_fft(m_tmp, subrange(vec.begin() + offset, vec.end()));
+            m_tmp = m_tmp * conj(m_kernel);
+            m_fft.ifft(m_tmp);
+            auto l_step = std::min(step, vec.size() - offset);
+            subrange(vec.begin() + offset, l_step).assign(subrange(m_tmp.begin(), l_step));
+            offset += step;
+        }
     }
 
 private:
