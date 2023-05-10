ebc1fb2c5e161c96060fd2c01f421ef73544c729
Timofey <timofey@umnik-22.04>
2023-05-10 15:21:54 +0300

    Updated fxcorr and test;

--- fxcorr.hpp
+++ fxcorr.hpp
@@ -1,64 +1,11 @@
 #include "fft.hpp"
 #include "vector_util.hpp"
 
+#include <concepts>
 #include <cstddef>
 #include <memory>
 
 namespace pcx {
-
-template<typename T,
-         std::size_t PackSize = pcx::default_pack_size<T>,
-         typename Allocator   = pcx::aligned_allocator<T>>
-class fxcorr_unit {
-    using real_type      = T;
-    using allocator_type = Allocator;
-
-public:
-    fxcorr_unit() = delete;
-
-    fxcorr_unit(pcx::vector<T> g, std::size_t fft_size, allocator_type allocator = allocator_type{})
-    : m_fft(fft_size, 2048, allocator)
-    , m_kernel(fft_size, allocator)
-    , m_tmp(fft_size, allocator)
-    , m_overlap(g.size() - 1) {
-        m_fft(m_kernel, g);
-        m_kernel = m_kernel / static_cast<T>(fft_size);
-    };
-
-    fxcorr_unit(const fxcorr_unit& other)     = default;
-    fxcorr_unit(fxcorr_unit&& other) noexcept = default;
-
-    ~fxcorr_unit() = default;
-
-    fxcorr_unit& operator=(const fxcorr_unit& other)     = delete;
-    fxcorr_unit& operator=(fxcorr_unit&& other) noexcept = delete;
-
-    template<typename VAllocator, std::size_t VPackSize>
-    void operator()(pcx::vector<T, VAllocator, VPackSize>& vec) {
-        uint offset = 0;
-        auto step   = (m_fft.size() - m_overlap) / VPackSize * VPackSize;
-        for (; offset + m_fft.size() < vec.size(); offset += step) {
-            m_fft(m_tmp, subrange(vec.begin() + offset, m_fft.size()));
-            m_tmp = m_tmp * conj(m_kernel);
-            m_fft.ifft(m_tmp);
-            subrange(vec.begin() + offset, step).assign(m_tmp);
-        }
-        while (offset < vec.size()) {
-            m_fft(m_tmp, subrange(vec.begin() + offset, vec.end()));
-            m_tmp = m_tmp * conj(m_kernel);
-            m_fft.ifft(m_tmp);
-            auto l_step = std::min(step, vec.size() - offset);
-            subrange(vec.begin() + offset, l_step).assign(subrange(m_tmp.begin(), l_step));
-            offset += step;
-        }
-    }
-
-private:
-    fft_unit<T, pcx::fft_output::unsorted, pcx::dynamic_size, pcx::dynamic_size, allocator_type> m_fft;
-    pcx::vector<T, allocator_type>                                                               m_kernel;
-    pcx::vector<T, allocator_type>                                                               m_tmp;
-    std::size_t                                                                                  m_overlap;
-};
 
 namespace internal {
 template<typename T, typename Allocator = pcx::aligned_allocator<T>>
@@ -84,12 +31,22 @@ private:
 };
 }    // namespace internal
 
+
+/**
+ * @brief 
+ * 
+ * @tparam T 
+ * @tparam Allocator 
+ * @tparam FFTUnit 
+ * @tparam TMPFactory A type with operator() returning a pointer_t. Dereferencing pointer_t must return pcx::vector<T, ...>&.
+   Size of the returned vector must be equal to fft size;
+ */
 template<typename T,
-         std::size_t PackSize = pcx::default_pack_size<T>,
-         typename Allocator   = pcx::aligned_allocator<T>,
+         typename Allocator = pcx::aligned_allocator<T>,
          typename FFTUnit =
              pcx::fft_unit<T, pcx::fft_output::unsorted, pcx::dynamic_size, pcx::dynamic_size, Allocator>,
          typename TMPFactory = internal::pseudo_vector_factory<T, Allocator>>
+    requires std::floating_point<T> && std::same_as<typename Allocator::value_type, T>
 class fxcorr_unit_shared {
 public:
     using real_type      = T;
@@ -99,10 +56,9 @@ public:
     fxcorr_unit_shared(pcx::vector<T> g, std::size_t fft_size, allocator_type allocator = allocator_type{})
     : m_fft(std::make_shared<FFTUnit>(fft_size, 2048, allocator))
     , m_kernel(m_fft->size(), allocator)
-    , m_tmp(m_fft->size(), allocator)
     , m_tmp_factory(m_fft->size(), allocator)
     , m_overlap(g.size() - 1) {
-        m_fft(m_kernel, g);
+        (*m_fft)(m_kernel, g);
         m_kernel = m_kernel / static_cast<T>(m_fft->size());
     };
 
@@ -111,26 +67,23 @@ public:
                        allocator_type           allocator = allocator_type{})
     : m_fft(std::move(fft_unit))
     , m_kernel(m_fft->size(), allocator)
-    , m_tmp(m_fft->size(), allocator)
     , m_tmp_factory(m_fft->size(), allocator)
     , m_overlap(g.size() - 1) {
-        m_fft(m_kernel, g);
+        (*m_fft)(m_kernel, g);
         m_kernel = m_kernel / static_cast<T>(m_fft->size());
     };
 
     fxcorr_unit_shared(pcx::vector<T>           g,
-                       std::shared_ptr<FFTUnit> fft_size,
+                       std::shared_ptr<FFTUnit> fft_unit,
                        TMPFactory               tmp_factory,
                        allocator_type           allocator = allocator_type{})
-    : m_fft(fft_size, 2048, allocator)
-    , m_kernel(fft_size, allocator)
-    , m_tmp(fft_size, allocator)
+    : m_fft(fft_unit)
+    , m_kernel(m_fft->size(), allocator)
     , m_tmp_factory(tmp_factory)
     , m_overlap(g.size() - 1) {
-        m_fft(m_kernel, g);
-        m_kernel = m_kernel / static_cast<T>(fft_size);
+        (*m_fft)(m_kernel, g);
+        m_kernel = m_kernel / static_cast<T>(m_fft->size());
     };
-
 
     fxcorr_unit_shared(const fxcorr_unit_shared& other)     = default;
     fxcorr_unit_shared(fxcorr_unit_shared&& other) noexcept = default;
@@ -145,17 +98,19 @@ public:
         uint offset = 0;
         auto step   = (m_fft->size() - m_overlap) / VPackSize * VPackSize;
         for (; offset + m_fft->size() < vec.size(); offset += step) {
-            (*m_fft)(m_tmp, subrange(vec.begin() + offset, m_fft->size()));
-            m_tmp = m_tmp * conj(m_kernel);
-            m_fft->ifft(m_tmp);
-            subrange(vec.begin() + offset, step).assign(m_tmp);
+            auto tmp = *m_tmp_factory();
+            (*m_fft)(tmp, subrange(vec.begin() + offset, m_fft->size()));
+            tmp = tmp * conj(m_kernel);
+            m_fft->ifft(tmp);
+            subrange(vec.begin() + offset, step).assign(tmp);
         }
         while (offset < vec.size()) {
-            (*m_fft)(m_tmp, subrange(vec.begin() + offset, vec.end()));
-            m_tmp = m_tmp * conj(m_kernel);
-            m_fft->ifft(m_tmp);
+            auto tmp = *m_tmp_factory();
+            (*m_fft)(tmp, subrange(vec.begin() + offset, vec.end()));
+            tmp = tmp * conj(m_kernel);
+            m_fft->ifft(tmp);
             auto l_step = std::min(step, vec.size() - offset);
-            subrange(vec.begin() + offset, l_step).assign(subrange(m_tmp.begin(), l_step));
+            subrange(vec.begin() + offset, l_step).assign(subrange(tmp.begin(), l_step));
             offset += step;
         }
     }
@@ -163,7 +118,6 @@ public:
 private:
     std::shared_ptr<FFTUnit>       m_fft;
     pcx::vector<T, allocator_type> m_kernel;
-    pcx::vector<T, allocator_type> m_tmp;
     TMPFactory                     m_tmp_factory;
     std::size_t                    m_overlap;
 };
--- tests/test_fxcorr.cpp
+++ tests/test_fxcorr.cpp
@@ -1,9 +1,18 @@
+#include "fft.hpp"
 #include "fxcorr.hpp"
+#include "vector_util.hpp"
+
+#include <memory>
 
 int main() {
     pcx::vector<float> g(1024);
     //
-    auto fconv = pcx::fxcorr_unit(g, 8192);
+    using fft_t   = pcx::fft_unit<float, pcx::fft_output::unsorted, pcx::dynamic_size, 2048>;
+    auto fft_unit = std::make_shared<fft_t>(8192);
+
+    auto pseudo_factory = pcx::internal::pseudo_vector_factory<float, std::allocator<float>>(8192);
+
+    auto fxcorr = pcx::fxcorr_unit_shared(g, fft_unit, [&] { return pseudo_factory(); });
 
     return 0;
 }
