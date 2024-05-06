a4b9bd8ae275b14a0d4b1f2a85c1232a7c442cd5
Timofey Dovgal <dooezzz@gmail.com>
2024-05-06 23:43:03 +0300

    Further generalized work.

--- inc/simd_fft_generalized.hpp
+++ inc/simd_fft_generalized.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "avx2_common.hpp"
+#include "fft.hpp"
 #include "simd_common.hpp"
 #include "simd_fft.hpp"
 #include "tuple_util.hpp"
@@ -187,17 +188,73 @@ struct newnode {
     };
 
     template<typename T, settings Settings, typename... Args>
-    static inline void perform(std::array<T*, NodeSize> dest, Args... args) {
+    static inline void perform(std::array<T*, NodeSize> dest, Args&&... args) {
         constexpr auto reg_size = simd::reg<T>::size;
         using cx_reg            = simd::cx_reg<T, false, reg_size>;
 
-        using src_type       = std::array<const T*, 4>;
-        using tw_type        = std::array<simd::cx_reg<T>, 3>;
+        using src_type       = std::array<const T*, NodeSize>;
+        using tw_type        = std::array<simd::cx_reg<T>, NodeSize - 1>;
         constexpr bool Src   = has_type<src_type, Args...>;
         constexpr bool Tw    = has_type<tw_type, Args...>;
         constexpr bool Scale = has_type<simd::reg_t<T>, Args...>;
 
-        constexpr auto& data_idx = order2<NodeSize, Settings.dit>::data;
+        constexpr auto load =
+            []<uZ... Is>(std::index_sequence<Is...>, const auto& dest, const auto&... args) {
+                constexpr auto& data_idx = order2<NodeSize, Settings.dit>::data;
+                if constexpr (Src) {
+                    auto& src = std::get<src_type&>(std::tie(args...));
+                    return std::make_tuple(simd::cxload<Settings.pack_src, reg_size>(src[data_idx[Is]])...);
+                } else {
+                    return std::make_tuple(simd::cxload<Settings.pack_src, reg_size>(dest[data_idx[Is]])...);
+                }
+            };
+
+        auto p0 = load(std::make_index_sequence<NodeSize>{}, dest, args...);
+        auto p1 = simd::inverse<Settings.conj_tw>(p0);
+
+        constexpr auto bfly = []<uZ L>(uZ_constant<L>, const auto& data, const auto& tw) {
+            constexpr uZ stride = NodeSize / powi(2, L);
+
+            constexpr auto get_half = []<uZ Start>(uZ_constant<Start>, const auto& data) {
+                return []<uZ... Grp>(std::index_sequence<Grp...>, const auto& data) {
+                    constexpr auto iterate = []<uZ... Iters, uZ Offset>(std::index_sequence<Iters...>,
+                                                                        uZ_constant<Offset>,
+                                                                        const auto& data) {
+                        return std::make_tuple(std::get<Start + Offset + Iters>(data)...);
+                    };
+                    return std::tuple_cat(iterate(std::make_index_sequence<stride / 2>{},    //
+                                                  uZ_constant<Grp * stride>{},
+                                                  data)...);
+                }(std::make_index_sequence<NodeSize / stride>{}, data);
+            };
+
+            auto bottom = get_half(uZ_constant<stride / 2>{}, data);
+
+            auto tws = []<uZ... Itw>(std::index_sequence<Itw...>, const auto& tw) {
+                constexpr auto make_rep =
+                    []<uZ... Reps, uZ I>(std::index_sequence<Reps...>, uZ_constant<I>, auto tw) {
+                        return std::make_tuple(((void)Reps, tw[powi(2UL, L) - 1 + I])...);
+                    };
+                return std::tuple_cat(make_rep(
+                    std::make_index_sequence<NodeSize / sizeof...(Itw)>{}, uZ_constant<Itw>{}, tw)...);
+            }(std::make_index_sequence<powi(2UL, L)>{}, tw);
+            auto bottom_tw = std::apply(simd::mul_pairs, zip_tuples(bottom, tws));
+            auto top       = get_half(uZ_constant<0>{}, data);
+
+            return mass_invoke(simd::btfly, top, bottom_tw);
+        };
+
+        auto res0 = []<uZ I>(uZ_constant<I>, const auto& data, const auto& tw) {
+            if constexpr (powi(2, I + 1) == NodeSize) {
+                return btfly(uZ_constant<I>{}, data, tw);
+            } else {
+                auto tmp = btfly(uZ_constant<I>{}, data, tw);
+                return btfly(uZ_constant<I + 1>{}, tmp, tw);
+            }
+        }(uZ_constant<0>{}, p1, std::get<tw_type&>(std::tie(args...)));
+        auto res1 = simd::inverse<Settings.conj_tw>(res0);
+
+
     }
 };
 
